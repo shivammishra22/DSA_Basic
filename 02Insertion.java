ae5ac09d94ccc180178a332a3e0cd177c439032a
Shivam Mishra <126499350+shivammishra22@users.noreply.github.com>
2026-05-26 15:15:46 +0530

    Add array insertion logic in ArrayInsertion2
    Implement array insertion functionality to add a new element at the beginning.

---
+++ 02Insertion.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayInsertion2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Enter Array Size:");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Enter Array Value");
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("enter the value to be inserted:");
+		int value=sc.nextInt();
+//		int [0]newArr=values;
+		
+		
+		int []newArr=new int[arr.length+1];
+		for(int i=0;i<arr.length;i++)
+		{
+			newArr[i+1]=arr[i];
+		}
+		newArr[0]=value;
+		arr=newArr;
+		System.out.println(Arrays.toString(arr));
+		sc.close();
+
+	}
+
+}
